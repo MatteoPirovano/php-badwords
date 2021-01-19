d334aed9c6771aa68d9fe0fdd3c9da9c94f58b7d
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2021-01-19 16:15:46 +0100

    Primo commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $text= "Ehi sai che sei un grandissimo stronzo?!";
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Badwords</title>
+</head>
+<body>
+  <h1>Badwords</h1>
+  <p><b>Testo:</b> <?php echo $text ?> ;</p>
+  <p><b>Lunghezza testo:</b> <?php echo strlen($text); ?> caratteri ;</p>
+  <p><b>Testo censurato:</b> <?php echo str_replace("stronzo", "***", $text) ?> ;</p>
+</body>
+</html>
